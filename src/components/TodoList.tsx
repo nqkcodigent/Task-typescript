90d4ecffe7c644c682bdadce4ebb82e129423d00
Khanh <nguyenquockhanhkz20@gmail.com>
2022-07-04 19:41:55 +0700

    update

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./styles.css";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+import { Droppable } from "react-beautiful-dnd";
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  return (
+    <div className="container">
+      <Droppable droppableId="TodosList">
+        {(provided) => (
+          <div
+            className="todos"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos__heading">Active Task</span>
+            {todos?.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todos={todos}
+                todo={todo}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <div className="todos remove">
+        <span className="todos__heading">Note Task</span>
+        <textarea
+          className="todos__single textarea--input"
+          rows={8}
+          cols={50}
+          placeholder="Writing something..."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
